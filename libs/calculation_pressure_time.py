5efe0883c965ffda3e70781a7947dee68679e039
tungyao <1922527051@qq.com>
2018-01-04 10:15:12 +0800

    first update

--- libs/calculation_pressure_time.py
+++ libs/calculation_pressure_time.py
@@ -3,10 +3,10 @@ class calcula:
     LENGTH = None
     PRESS_TIME =None
     def run(self):
-        a = self.LENGTH*0.4*self.LENGTH*1.35
+        a =(self.LENGTH+1)*1.36535*14
         print("INFO calculate time is %dms"%a)
         return int(a)
 if __name__ =="__main__":
     a = calcula()
-    a.LENGTH =35
+    a.LENGTH =40
     a.run()
